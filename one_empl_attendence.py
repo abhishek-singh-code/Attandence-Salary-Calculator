6d19338cc6fc027463b8ef9433047db1e6bdc313
Abhishek Singh <as4571021@gmail.com>
2026-01-27 21:08:47 +0530

    Updated employee attendance logic

--- one_empl_attendence.py
+++ one_empl_attendence.py
@@ -1,15 +1,23 @@
-name = input("Enter employee name: ")
-total_working_day = int(input("Enter total working days: "))
-total_present_day = int(input("Enter total present days: "))
-salary_per_day = int(input("Enter salary per day: "))
+name = input("Enter Employee Name: ")
+monthly_salary = int(input("Enter monthly salary of employee: "))
+working_day = 26
+present_day = int(input("Enter total present days: "))
 
-if(total_present_day>total_working_day):
+if(present_day>working_day):
     print("Error present day cannot be more than working day")
 else:
-    salary = total_present_day*salary_per_day
+    absent_days = working_day - present_day
+    per_day_salary = monthly_salary/working_day
+    deduction = round(absent_days*per_day_salary)
+    final_salary = monthly_salary-deduction
+
     print("\n--- Employee Salary Details ---")
     print("Name: ",name)
-    print("Present days: ",total_present_day)
-    print("Total Salary: ₹",salary)
+    print("Monthly Salary: ₹",monthly_salary)
+    print("Working Days: ",working_day)
+    print("Present days: ",present_day)
+    print("Absent days: ",absent_days)
+    print("Salary Deduction: ",(deduction))
+    print("Final Salary: ₹",(final_salary))
 
 
