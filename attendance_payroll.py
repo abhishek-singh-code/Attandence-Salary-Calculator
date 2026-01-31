69bb412e285dd279bb5e53f8d8290ed8d7aaf020
Abhishek Singh <as4571021@gmail.com>
2026-01-31 22:02:52 +0530

    Implement employee ID lookup with pre-stored salary details

--- attendance_payroll.py
+++ attendance_payroll.py
@@ -1,38 +1,62 @@
-name = input("Enter Employee Name: ")
-monthly_salary = int(input("Enter Monthly Salary of Employee: "))
-absent_days = int(input("Enter Total Absent Days: "))
+
+employee = {
+        101:{"Name": "Santosh Paswan", "Salary": 18000},
+        102:{"Name": "Mithun", "Salary": 15000},
+        103:{"Name": "Birender", "Salary": 17500}
+
+};
 
 working_day = 26
 duty_hours = 8
-present_day = working_day - absent_days
-per_day_pay = monthly_salary/working_day
-per_hour_pay = per_day_pay/duty_hours
 
+emp_id = int(input("Enter Employee Id: "))
+if(emp_id not in employee):
+    print("❌ Error: Employee ID not found")
+else:
+    name = employee[emp_id]["Name"]
+    monthly_salary = employee[emp_id]["Salary"]
+
+    print(f"\nEmployee Name   : {name}")
+    print(f"Monthly Salary  : ₹{monthly_salary}")
+
+absent_days = int(input("Enter Total Absent Days: "))
 
 if(absent_days>working_day or absent_days<0):
-    print("Error: Absent days must be between 0 and 26")
+    print("\nError: Absent days must be between 0 and 26")
 
 else:
     overtime_hour = int(input("Enter Overtime Hours: "))
     if(overtime_hour<0):
-        print("Error : Overtime Hour cannot be negative")
+        print("\nError : Overtime Hour cannot be negative")
     else:
+        present_day = working_day - absent_days
+        per_day_pay = monthly_salary/working_day
+        per_hour_pay = per_day_pay/duty_hours
+
         deduction = round(absent_days*per_day_pay)
         overtime_pay = round(overtime_hour * per_hour_pay * 1.5)
         final_salary = monthly_salary-deduction+overtime_pay
-        print("\n--- Employee Salary Details ---")
-        print("Name: ",name)
-        print("Monthly Salary: ₹",monthly_salary)
-        print("Working Days: ",working_day)
-        print("Present Days: ",present_day)
-        if(absent_days>0):
-            print("Absent Days: ",absent_days)
-            print("Salary Deduction: ",(deduction))
-        if(overtime_hour>0):
-            print("Overtime Hours: ",overtime_hour)
-            print("Overtime Pay: ",overtime_pay)
+
+        print("\n" + "-" * 35)
+        print("     Employee Salary Details")
+        print("-" * 35)
+
+        print(f"Employee Name     : {name}")
+        print(f"Monthly Salary    : ₹{monthly_salary}")
+        print(f"Working Days      : {working_day}")
+        print(f"Present Days      : {present_day}")
         
-        print("Final Salary: ₹",(final_salary))
+        if(absent_days>0):
+            print(f"Absent Days       : {absent_days}")
+            print(f"Salary Deduction  : {deduction}")
+        
+        if(overtime_hour>0):
+            print(f"Overtime Hours    : {overtime_hour}")
+            print(f"Overtime Pay      : {overtime_pay}")
+        print("-" * 35)
+        print(f"Final Salary      : ₹{final_salary}")
+        print("-" * 35)
+
 
 
 
